3dc91cf10e2801ea3e21c00cfe306f59c10542df
IAN CHEN <ianchen0419@gmail.com>
2018-05-21 00:20:45 +0800

    Top page

--- inc/js/base.js
+++ inc/js/base.js
@@ -2,13 +2,15 @@ const menuChildHeads=document.querySelectorAll('.child-head span');
 
 menuChildHeads.forEach(head => head.addEventListener('click', function(){
  const nowActived=document.querySelector('.child-menu.active');
+ console.log(this.nextElementSibling);
+
  if(nowActived){
   nowActived.classList.remove("active");
  }
- 
- if(nowActived==this.children[0]){
-  this.children[0].classList.remove("active");
+
+ if(nowActived==this.nextElementSibling){
+   this.nextElementSibling.classList.remove("active");
  }else{
-   this.children[0].classList.add("active");
+   this.nextElementSibling.classList.add("active");
  }
 }))
