13af35d9f059f7307584bcf762033ce486b50453
YianChen <ianchen0419@gmail.com>
2018-05-22 22:54:03 +0800

    top-page motion

--- inc/js/base.js
+++ inc/js/base.js
@@ -1,16 +1,42 @@
 const menuChildHeads=document.querySelectorAll('.child-head span');
 
 menuChildHeads.forEach(head => head.addEventListener('click', function(){
- const nowActived=document.querySelector('.child-menu.active');
- console.log(this.nextElementSibling);
+	const nowActived=document.querySelector('.child-menu.active');
+	console.log(this.nextElementSibling);
 
- if(nowActived){
-  nowActived.classList.remove("active");
- }
+	if(nowActived){
+		nowActived.classList.remove("active");
+	}
 
- if(nowActived==this.nextElementSibling){
-   this.nextElementSibling.classList.remove("active");
- }else{
-   this.nextElementSibling.classList.add("active");
- }
-}))
+	if(nowActived==this.nextElementSibling){
+		this.nextElementSibling.classList.remove("active");
+	}else{
+		this.nextElementSibling.classList.add("active");
+	}
+}));
+
+
+header.style.display="none";
+menu.style.display="none";
+visual.style.display="none";
+contact.style.display="none";
+footer.style.display="none";
+document.body.classList.add('motion-body');
+
+setTimeout(function(){ 
+	document.body.classList.remove('motion-body');
+
+	header.style.display="";
+	menu.style.display="";
+	visual.style.display="";
+	contact.style.display="";
+	footer.style.display="";
+
+	header.classList.add('showing');
+	menu.classList.add('showing');
+	visual.classList.add('showing');
+	contact.classList.add('showing');
+	footer.classList.add('showing');
+
+
+}, 3000);
